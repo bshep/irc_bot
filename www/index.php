1a2e5591d094ea19d2f0908bae17917dee372d3c
Bruce Sheplan <git.bshep@xoxy.net>
2011-05-13 12:44:31 -0400

    - Simplified code

--- www/index.php
+++ www/index.php
@@ -1,14 +1,6 @@
 <?php
-//phpinfo();
-    error_reporting(E_ALL);
-    ini_set("display_errors","On");
-    ini_set('safe_mode_exec_dir', '/usr/bin/');
-    print 'ExecDir: '.ini_get('safe_mode_exec_dir');
-
     chdir('..');
-$out = array();
-$ret = exec('git pull', $out);
-print_r($out);
-print 'RetVal:'.$ret;
-
+    $out = array();
+    exec('git pull', $out);
+    print_r($out);
 ?>
