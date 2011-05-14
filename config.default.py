a248b013ef32cffad5d00c49645c9febfd4a40e3
Bruce Sheplan <git.bshep@xoxy.net>
2011-05-14 13:09:47 -0400

    - Default configuration file

---
+++ config.default.py
@@ -0,0 +1,8 @@
+HOST='irc.server.net' #The server we want to connect to
+PORT=6667 #The connection port which is usually 6667
+NICK='NickName' #The bot's nickname
+IDENT='IdentName'
+REALNAME='RealName'
+channel_list='#chan1 #chan2' #The default channel for the bot
+
+MODULES_LOAD='urldb whois'
